9c90dbf0e2cf418751480e014709e2c487dc74b4
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2026-03-25 16:38:44 -0300

    Primer commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    react(), // <--- ESTO ES CLAVE para que reconozca tus componentes .jsx
     tailwindcss(),
   ],
+  server: {
+    // Esto ayuda a que el front sepa dónde buscar si tenés problemas de rutas
+    fs: {
+      allow: ['..']
+    }
+  }
 })
