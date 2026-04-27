b6be1a2e6c51bd6dac390102dd8383a0b9bce175
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2026-04-27 12:18:11 -0300

    final5

--- demo/src/main/java/opticamolina/demo/config/SecurityConfig.java
+++ demo/src/main/java/opticamolina/demo/config/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-                "https://opticamolinacba.vercel.app"
+                "https://opticamolinacba.vercel.app",
+                "https://opticamolinashop.com"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList(
