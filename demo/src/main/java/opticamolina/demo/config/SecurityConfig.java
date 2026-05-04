7023c8ba4be8dace7dd0c355e71a6bbe763870cc
ALEJO200244346760 <ALEJOJORJA@GMAIL.COM>
2026-05-04 18:00:13 -0300

    dar vuelta

--- demo/src/main/java/opticamolina/demo/config/SecurityConfig.java
+++ demo/src/main/java/opticamolina/demo/config/SecurityConfig.java
@@ -84,7 +84,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/public/**").permitAll()
                         .requestMatchers("/api/payments/**").permitAll()
-                        .requestMatchers("/api/admin/**").hasAnyAuthority("ROLE_ADMIN", "ADMIN")
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/public/products/**").permitAll()
                         .anyRequest().authenticated()
                 );
